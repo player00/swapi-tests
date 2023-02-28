7a5c0813a1c82cdd8d2b361f9e65466912dc9bd6
shalygins <shalygins23@gmail.com>
2023-02-28 21:11:36 +0300

    added annotations

--- utils/api_client.py
+++ utils/api_client.py
@@ -2,7 +2,7 @@ import requests
 
 
 class ApiClient():
-    def __init__(self, base_url):
+    def __init__(self, base_url: str):
         self.base_url = base_url
 
     def ping(self):
@@ -10,26 +10,26 @@ class ApiClient():
         response.raise_for_status()
         return response.json()
 
-    def get_resource_by_id(self, resource, id):
+    def get_resource_by_id(self, resource: str, id: str):
         response = requests.get(self.base_url + f"{resource}/{id}/")
         response.raise_for_status()
         return response.json()
 
-    def search_for_resource(self, resource, data):
+    def search_for_resource(self, resource: str, data: dict):
         response = requests.get(self.base_url + f"{resource}/?search={data}")
         response.raise_for_status()
         return response.json()
 
-    def create_new_resource(self, resource, data):
+    def create_new_resource(self, resource: str, data: dict):
         response = requests.post(self.base_url + f"{resource}/{data}")
         response.raise_for_status()
         return response.json()
 
-    def update_existing_resource(self, resource, id, data):
+    def update_existing_resource(self, resource: str, id: str, data: dict):
         response = requests.put(self.base_url + f"{resource}/{id}/", data=data)
         response.raise_for_status()
         return response.json()
-    
+
     def delete_existing_resource(self, resource, id):
         response = requests.delete(self.base_url + f"{resource}/{id}/")
         response.raise_for_status()
